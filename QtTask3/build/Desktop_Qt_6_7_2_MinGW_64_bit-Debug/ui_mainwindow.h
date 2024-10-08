f8f427154cd2cb28f912abfce2b95a1a3b0fc219
Suroof <120703600+Suroof@users.noreply.github.com>
2024-10-08 21:32:02 +0800

    Add files via upload
    QtTask3

---
+++ QtTask3/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,238 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.7.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QLineEdit *lineEdit;
+    QPushButton *button1;
+    QPushButton *button2;
+    QPushButton *button3;
+    QPushButton *button6;
+    QPushButton *button5;
+    QPushButton *button4;
+    QPushButton *buttonMultiply;
+    QPushButton *buttonSubtract;
+    QPushButton *button7;
+    QPushButton *button8;
+    QPushButton *button9;
+    QPushButton *buttonClear;
+    QPushButton *pushButton_13;
+    QPushButton *button0;
+    QPushButton *buttonDecimal;
+    QPushButton *buttonEquals;
+    QPushButton *buttonAdd;
+    QPushButton *buttonDivide;
+    QPushButton *buttonBackspace;
+    QPushButton *pushButton_20;
+    QPushButton *pushButton_21;
+    QPushButton *buttonClear_2;
+    QPushButton *buttonClear_3;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(1301, 736);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName("lineEdit");
+        lineEdit->setGeometry(QRect(170, 80, 301, 41));
+        lineEdit->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;\n"
+""));
+        button1 = new QPushButton(centralwidget);
+        button1->setObjectName("button1");
+        button1->setGeometry(QRect(170, 260, 61, 51));
+        button1->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        button2 = new QPushButton(centralwidget);
+        button2->setObjectName("button2");
+        button2->setGeometry(QRect(250, 260, 61, 51));
+        button2->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        button3 = new QPushButton(centralwidget);
+        button3->setObjectName("button3");
+        button3->setGeometry(QRect(330, 260, 61, 51));
+        button3->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        button6 = new QPushButton(centralwidget);
+        button6->setObjectName("button6");
+        button6->setGeometry(QRect(330, 320, 61, 51));
+        button6->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        button5 = new QPushButton(centralwidget);
+        button5->setObjectName("button5");
+        button5->setGeometry(QRect(250, 320, 61, 51));
+        button5->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        button4 = new QPushButton(centralwidget);
+        button4->setObjectName("button4");
+        button4->setGeometry(QRect(170, 320, 61, 51));
+        button4->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        buttonMultiply = new QPushButton(centralwidget);
+        buttonMultiply->setObjectName("buttonMultiply");
+        buttonMultiply->setGeometry(QRect(410, 200, 61, 51));
+        buttonMultiply->setStyleSheet(QString::fromUtf8("background-color:#e5f8ff;\n"
+"border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        buttonSubtract = new QPushButton(centralwidget);
+        buttonSubtract->setObjectName("buttonSubtract");
+        buttonSubtract->setGeometry(QRect(410, 260, 61, 51));
+        buttonSubtract->setStyleSheet(QString::fromUtf8("background-color:#e5f8ff;\n"
+"border: 2px solid #8f8f91;\n"
+"border-radius: 10px;background-color:#e5f8ff;"));
+        button7 = new QPushButton(centralwidget);
+        button7->setObjectName("button7");
+        button7->setGeometry(QRect(170, 380, 61, 51));
+        button7->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        button8 = new QPushButton(centralwidget);
+        button8->setObjectName("button8");
+        button8->setGeometry(QRect(250, 380, 61, 51));
+        button8->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        button9 = new QPushButton(centralwidget);
+        button9->setObjectName("button9");
+        button9->setGeometry(QRect(330, 380, 61, 51));
+        button9->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        buttonClear = new QPushButton(centralwidget);
+        buttonClear->setObjectName("buttonClear");
+        buttonClear->setGeometry(QRect(170, 140, 61, 51));
+        buttonClear->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        pushButton_13 = new QPushButton(centralwidget);
+        pushButton_13->setObjectName("pushButton_13");
+        pushButton_13->setGeometry(QRect(170, 440, 61, 51));
+        pushButton_13->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        button0 = new QPushButton(centralwidget);
+        button0->setObjectName("button0");
+        button0->setGeometry(QRect(250, 440, 61, 51));
+        button0->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        buttonDecimal = new QPushButton(centralwidget);
+        buttonDecimal->setObjectName("buttonDecimal");
+        buttonDecimal->setGeometry(QRect(330, 440, 61, 51));
+        buttonDecimal->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        buttonEquals = new QPushButton(centralwidget);
+        buttonEquals->setObjectName("buttonEquals");
+        buttonEquals->setGeometry(QRect(410, 380, 61, 111));
+        buttonEquals->setStyleSheet(QString::fromUtf8("color:rgb(0, 0, 0);\n"
+"background-color:#3ca4ff;\n"
+"border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        buttonAdd = new QPushButton(centralwidget);
+        buttonAdd->setObjectName("buttonAdd");
+        buttonAdd->setGeometry(QRect(410, 320, 61, 51));
+        buttonAdd->setStyleSheet(QString::fromUtf8("background-color:#e5f8ff;\n"
+"border: 2px solid #8f8f91;\n"
+"border-radius: 10px;background-color:#e5f8ff;"));
+        buttonDivide = new QPushButton(centralwidget);
+        buttonDivide->setObjectName("buttonDivide");
+        buttonDivide->setGeometry(QRect(330, 200, 61, 51));
+        buttonDivide->setStyleSheet(QString::fromUtf8("background-color:#e5f8ff;\n"
+"border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        buttonBackspace = new QPushButton(centralwidget);
+        buttonBackspace->setObjectName("buttonBackspace");
+        buttonBackspace->setGeometry(QRect(410, 140, 61, 51));
+        buttonBackspace->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        pushButton_20 = new QPushButton(centralwidget);
+        pushButton_20->setObjectName("pushButton_20");
+        pushButton_20->setGeometry(QRect(250, 140, 61, 51));
+        pushButton_20->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        pushButton_21 = new QPushButton(centralwidget);
+        pushButton_21->setObjectName("pushButton_21");
+        pushButton_21->setGeometry(QRect(330, 140, 61, 51));
+        pushButton_21->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        buttonClear_2 = new QPushButton(centralwidget);
+        buttonClear_2->setObjectName("buttonClear_2");
+        buttonClear_2->setGeometry(QRect(250, 200, 61, 51));
+        buttonClear_2->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        buttonClear_3 = new QPushButton(centralwidget);
+        buttonClear_3->setObjectName("buttonClear_3");
+        buttonClear_3->setGeometry(QRect(170, 200, 61, 51));
+        buttonClear_3->setStyleSheet(QString::fromUtf8("border: 2px solid #8f8f91;\n"
+"border-radius: 10px;"));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 1301, 25));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        button1->setText(QCoreApplication::translate("MainWindow", "1", nullptr));
+        button2->setText(QCoreApplication::translate("MainWindow", "2", nullptr));
+        button3->setText(QCoreApplication::translate("MainWindow", "3", nullptr));
+        button6->setText(QCoreApplication::translate("MainWindow", "6", nullptr));
+        button5->setText(QCoreApplication::translate("MainWindow", "5", nullptr));
+        button4->setText(QCoreApplication::translate("MainWindow", "4", nullptr));
+        buttonMultiply->setText(QCoreApplication::translate("MainWindow", "*", nullptr));
+        buttonSubtract->setText(QCoreApplication::translate("MainWindow", "-", nullptr));
+        button7->setText(QCoreApplication::translate("MainWindow", "7", nullptr));
+        button8->setText(QCoreApplication::translate("MainWindow", "8", nullptr));
+        button9->setText(QCoreApplication::translate("MainWindow", "9", nullptr));
+        buttonClear->setText(QCoreApplication::translate("MainWindow", "MC", nullptr));
+        pushButton_13->setText(QCoreApplication::translate("MainWindow", "+/-", nullptr));
+        button0->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        buttonDecimal->setText(QCoreApplication::translate("MainWindow", ".", nullptr));
+        buttonEquals->setText(QCoreApplication::translate("MainWindow", "=", nullptr));
+        buttonAdd->setText(QCoreApplication::translate("MainWindow", "+", nullptr));
+        buttonDivide->setText(QCoreApplication::translate("MainWindow", "/", nullptr));
+        buttonBackspace->setText(QCoreApplication::translate("MainWindow", "\342\235\214", nullptr));
+        pushButton_20->setText(QCoreApplication::translate("MainWindow", "CE", nullptr));
+        pushButton_21->setText(QCoreApplication::translate("MainWindow", "%", nullptr));
+        buttonClear_2->setText(QCoreApplication::translate("MainWindow", "M-", nullptr));
+        buttonClear_3->setText(QCoreApplication::translate("MainWindow", "M+", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
