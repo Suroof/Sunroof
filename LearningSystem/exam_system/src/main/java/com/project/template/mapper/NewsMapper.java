6aac45d150013f3d6f469137122a07a454c661b2
Suroof <120703600+Suroof@users.noreply.github.com>
2024-10-11 12:11:42 +0800

    Delete LearningSystem/exam_system directory

--- LearningSystem/exam_system/src/main/java/com/project/template/mapper/NewsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.project.template.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.project.template.entity.News;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface NewsMapper extends BaseMapper<News> {
-}
